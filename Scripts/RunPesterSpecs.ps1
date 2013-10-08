50a6b16e56f9939ff77c6e2fce3da5e8b27bf322
rouanw <rouanw@gmail.com>
2013-10-08 13:04:15 +0200

    add install psget step

--- Scripts/RunPesterSpecs.ps1
+++ Scripts/RunPesterSpecs.ps1
@@ -1,3 +1,5 @@
+echo "Importing PSGet"
+(new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 Import-Module PsGet
 echo "Installing Pester"
 Install-Module Pester -Destination ../WindowsPowerShell/Modules
