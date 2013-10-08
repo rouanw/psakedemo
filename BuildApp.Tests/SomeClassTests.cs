4b15ce936f6f41470ec7691bc57b0d6860c95151
rouanw <rouanw@gmail.com>
2013-10-08 11:45:24 +0200

    failing test

---
+++ BuildApp.Tests/SomeClassTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildApp.Tests
+{
+    [TestClass]
+    public class SomeClassTests
+    {
+        [TestMethod]
+        public void ItIsTrue()
+        {
+            true.Should().BeFalse("Cause I said so");
+        }
+    }
+}
